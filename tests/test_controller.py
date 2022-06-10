a475b02dc8c4ab5fbc48b82719ca18c65449b5cc
Thibaut Lamadon <thibaut.lamadon@gmail.com>
2022-06-09 19:29:09 -0500

    added a first test for retry

--- tests/test_controller.py
+++ tests/test_controller.py
@@ -56,3 +56,39 @@ def test_controller_init(event_loop):
     controller.add_completed(task2)
     task2.set_completed.assert_called()
 
+def test_controller_retry(event_loop):
+
+    task1 = Mock(**{
+        'output_file' :"tmp.txt",
+        'props' : {},
+        'deps':[],
+        'retry':1,
+        'fut': event_loop.create_future(),
+        'hash': "somehash",
+        'get_command.return_value' :  "bla bla",
+        'get_outputs.return_value' :  ['tmp.txt']})
+
+    runner = Mock(**{
+        'available_slots.return_value' : 1})
+
+    controller = sf.core.Controller(runner=runner)
+
+    controller.add(task1)
+    task1.assert_not_called()
+    controller.update()
+
+    # at this point the runner should have assigned the task
+    runner.add.assert_called()
+
+    # we then call complete on the task
+    controller.add_completed(task1)
+    # here we should fail because output file is missing
+    assert task1.retry == 0
+
+    # the following should call the runner again
+    controller.update()
+    controller.add_completed(task1)
+
+    # check that task was completed
+    task1.set_completed.assert_called()
+    controller.add_completed(task1)
